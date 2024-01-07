787114048f2e0f5a6375f87471c55d9567e895c7
Ayush_ps <ayushpratap9528@gmail.com>
2024-01-07 18:52:50 +0530

    First-Commit

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import logo from './logo.svg';
+import './App.css';
+import Header from './Components/HeaderFooter/Header';
+import Footer from './Components/HeaderFooter/Footer';
+import { BrowserRouter as Router } from "react-router-dom";
+import AnimatedRoutes from "./Components/helpers/AnimatedRoutes";
+function App() {
+  return (
+    <>
+      <Router>
+        <Header />
+        <AnimatedRoutes />
+        <Footer />
+      </Router>
+    </>
+  );
+}
+
+export default App;
---
+++ src/Components/helpers/AnimatedRoutes.jsx
@@ -0,0 +1,23 @@
+import { Route, Routes, useLocation } from "react-router-dom";
+import ScheduleMeeting from "../Pages/ScheduleMeeting";
+import PeriodTracker from "../Pages/PeriodTracker";
+import AppointmentForm from "../Pages/AppointmentForm";
+import { AnimatePresence } from "framer-motion";
+import React from 'react';
+
+const AnimatedRoutes = () => {
+  const location = useLocation();
+  return (
+    <AnimatePresence>
+      <Routes location={location} key={location.pathname}>
+        {/* <Route path="/" element={<Home/>}/> */}
+        <Route path="/AppointmenForm" element={<AppointmentForm/>} />
+        <Route path="/ScheduleMeeting" element={<ScheduleMeeting/>} />
+        <Route path="/PeriodTracker" element={<PeriodTracker/>} />
+  
+      </Routes>
+    </AnimatePresence>
+  );
+};
+
+export default AnimatedRoutes;
